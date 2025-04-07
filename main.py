701787db60b0766f296e8fd24daa283677c7a1be
Pavel <p3543815@icloud.com>
2025-04-06 21:17:59 -0700

    initial commit

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-import purchase_analysis as pa
+from datetime import datetime as dt
+
 import data
+import purchase_analysis as pa
 
 
 def main():
     # TODO Реализовать создание файла с отчетом. Отчет необходимо вывести и терминал и сохранить в файле report.txt
     price = int(input("Введите значение для отчета: 'Список покупок, где цена превышает заданное значение.': "))
-    print()
-    print(f"Общая выручка: {pa.total_revenue(data.purchases)}")
-    print(f"Товары по категориям: {pa.items_by_category(data.purchases)}")
-    print(f"Покупки дороже {price:.1f}: {pa.expensive_purchases(data.purchases, price)}")
-    print(f"Средняя цена по категориям: {pa.average_price_by_category(data.purchases)}")
-    print(f"Категория с наибольшим количеством проданных товаров: {pa.most_frequent_category(data.purchases)}")
+    current_datetime = dt.now()
+    reports = [
+    "\n***",
+    f"Отчет создан: {current_datetime.strftime("%d.%m.%Y %H:%M:%S")}",
+    "***",
+    f"Общая выручка: {pa.total_revenue(data.purchases)}",
+    f"Товары по категориям: {pa.items_by_category(data.purchases)}",
+    f"Покупки дороже {price:.1f}: {pa.expensive_purchases(data.purchases, price)}",
+    f"Средняя цена по категориям: {pa.average_price_by_category(data.purchases)}",
+    f"Категория с наибольшим количеством проданных товаров: {pa.most_frequent_category(data.purchases)}"
+    ]
+    for report in reports:
+        print(report)
+    try:
+        with open("report.txt", "a", encoding="UTF-8") as file:
+            for report in reports:
+                file.writelines(f"{report}\n")
+    except Exception as err:
+        print(f"Ошибка создания файла: {err}")
+    print("Отчет сохранен в файл report.txt")
 
 
 if __name__ == "__main__":
