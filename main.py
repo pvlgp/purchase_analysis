61431f4419ca7e3092271aaf3708c94ebb5e1da6
Pavel <p3543815@icloud.com>
2025-04-07 16:10:08 -0700

    initial commit

--- main.py
+++ main.py
@@ -5,28 +5,36 @@ import purchase_analysis as pa
 
 
 def main():
-    # TODO Реализовать создание файла с отчетом. Отчет необходимо вывести и терминал и сохранить в файле report.txt
     price = int(input("Введите значение для отчета: 'Список покупок, где цена превышает заданное значение.': "))
     current_datetime = dt.now()
-    reports = [
-    "\n***",
-    f"Отчет создан: {current_datetime.strftime("%d.%m.%Y %H:%M:%S")}",
-    "***",
-    f"Общая выручка: {pa.total_revenue(data.purchases)}",
-    f"Товары по категориям: {pa.items_by_category(data.purchases)}",
-    f"Покупки дороже {price:.1f}: {pa.expensive_purchases(data.purchases, price)}",
-    f"Средняя цена по категориям: {pa.average_price_by_category(data.purchases)}",
-    f"Категория с наибольшим количеством проданных товаров: {pa.most_frequent_category(data.purchases)}"
-    ]
-    for report in reports:
-        print(report)
+    try:
+        reports = {
+        "Отчет создан": current_datetime.strftime("%d.%m.%Y %H:%M:%S"),
+        "Общая выручка": pa.total_revenue(data.purchases),
+        "Товары по категориям": pa.items_by_category(data.purchases),
+        f"Покупки дороже {price:.1f}": pa.expensive_purchases(data.purchases, price),
+        "Средняя цена по категориям": pa.average_price_by_category(data.purchases),
+        "Категория с наибольшим количеством проданных товаров": pa.most_frequent_category(data.purchases)
+        }
+    except Exception as err:
+        print(f"Ошибка формирования отчета: {err}")
+    print()
+    try:
+        for key, value in reports.items():
+            print(f"{key}: {value}")
+    except Exception as err:
+        print(f"Ошибка вывода отчета: {err}")
     try:
         with open("report.txt", "a", encoding="UTF-8") as file:
-            for report in reports:
-                file.writelines(f"{report}\n")
+            file.write("\n")
+            for key, value in reports.items():
+                file.writelines(f"{key}: {value}\n")
+            print("***Отчет сохранен в файл report.txt***")
     except Exception as err:
         print(f"Ошибка создания файла: {err}")
-    print("Отчет сохранен в файл report.txt")
+    end_time = dt.now()
+    lead_time = end_time - current_datetime
+    print(f"Затрачено времени {lead_time}")
 
 
 if __name__ == "__main__":
--- purchase_analysis.py
+++ purchase_analysis.py
@@ -9,12 +9,18 @@ def items_by_category(purchases) -> dict:
     '''Формирование списка товаров по категориям'''
     category = dict()
     for i in purchases:
-        for _ in i:
-            pass
+        for key, value in i.items():
+            if key == 'category':
+                if value in category:
+                    category[value] = ''
 
-def expensive_purchases(purchases, min_price):
+def expensive_purchases(purchases, min_price) -> list:
     '''Вывод списка товаров, где цена превышает заданное значение'''
-    pass
+    list_purchases = []
+    for i in purchases:
+        if i['price'] > min_price:
+            list_purchases.append(i)
+    return list_purchases
 
 def average_price_by_category(purchases):
     '''Вывод средней цены товаров по категориям'''
